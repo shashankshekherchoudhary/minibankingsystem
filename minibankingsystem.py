9207b6e196702202eb4bec6d5f6dfdd4d1cd8092
Shashank <cshashankshekher@gmail.com>
2026-01-17 01:58:49 +0530

    Add create account feature

--- minibankingsystem.py
+++ minibankingsystem.py
@@ -2,9 +2,27 @@ from random import randint
 accounts = []
 def generate_account_number():
     while True:
-        acc_no = randint(100000, 999999)
+        acc_num = randint(100000, 999999)
         for account in accounts:
-            if account["acc_no"] == acc_no:
+            if account["acc_num"] == acc_num:
                 break
         else:
-            return acc_no
+            return acc_num
+
+#Create account
+def create_account():
+    account_number = generate_account_number()
+    name = input("Enter account holder name : ")
+    minimum_balance = int(input("Deposit minimum balance (Min ₹500) : "))
+    contact_number = input("Enter contact number : ")
+
+    account = {
+        'acc_num' : account_number ,
+        'name' : name ,
+        'balance' : minimum_balance ,
+        'contact_number' : contact_number
+                }
+    
+    accounts.append(account)
+
+
